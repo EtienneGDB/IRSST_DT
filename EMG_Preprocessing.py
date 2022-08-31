53d90b8954ff68fd2d25c49cf796b042e3ffe17a
EtienneGoubault <etienne.goubault@yahoo.fr>
2022-08-31 16:51:29 -0400

    Modified script corresponding to Extract_Raw_EMG saved modifications

--- EMG_Preprocessing.py
+++ EMG_Preprocessing.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 from pyomeca import Analogs
-# import time
+import pickle
+import time
 
 # Define variables
 Participants = ['Pilote' + str(iP) for iP in range(1, 25)] # Define Participants using comprehension list
@@ -13,83 +14,84 @@ EMG_Freq = 2000 # Acquisition frequency 2000 Hz
 iP = 2
 
 # ---Import File---
-# Import .mat file
-StrucData = sio.loadmat("J:/IRSST_DavidsTea/Data_exported/EMG/Python/Raw/" + Participants[iP] + "_EMG_raw.mat")
-Keys = [key for key in StrucData if key.startswith('Trial')]
+start = time.time()
+with open('test_pick', 'rb') as f:
+    StrucData = pickle.load(f)
+end = time.time()
+print('Time to load pickle', end-start)
+Keys = StrucData.keys()
 
-for iKeys in range(len(Keys)):
-    Data_mat = np.array([StrucData[Muscles[iM]] for iM in range(len(Muscles))])
-    EMG = Analogs(Data_mat[:, 0, :])
+for key in Keys:
+    # Data_mat = np.array([StrucData[key][Muscles[iM]] for iM in range(len(Muscles))])
+    # EMG = Analogs(Data_mat[:, :])
+    EMG = Analogs(np.array([StrucData[key][Muscle] for Muscle in Muscles]))
     EMG['channel'] = Muscles
 
-# # Import .c3d raw file (this is longer than opening a .mat file)
-# data_path = "J:/IRSST_Fatigue/Pointage_repetitif/Data/" + Participants[iP] + "/Trial/Pointage.c3d"
-# EMG = Analogs.from_c3d(data_path, suffix_delimiter=".", usecols=Muscles)
+    # ---Raw signals---
+    fft_EMG = EMG.meca.fft(freq=EMG_Freq)
 
-# ---Raw signals---
-fft_EMG = EMG.meca.fft(freq=EMG_Freq)
+    EMG.plot(x="time", col="channel", col_wrap=5)
+    plt.suptitle('Raw EMG signals' + key, fontsize=16)
 
-EMG.plot(x="time", col="channel", col_wrap=5)
-plt.suptitle('Raw EMG signals', fontsize=16)
+    plt.figure(2)
+    plt.suptitle('FFT Raw EMG signals' + key, fontsize=16)
+    for iM in range(len(Muscles)):
+        plt.subplot(2, 5, iM + 1)
+        fft_EMG[iM].plot.line(x="freq")
+        plt.title(Muscles[iM])
 
-plt.figure(2)
-plt.suptitle('FFT Raw EMG signals', fontsize=16)
-for iM in range(len(Muscles)):
-    plt.subplot(2, 5, iM + 1)
-    fft_EMG[iM].plot.line(x="freq")
-    plt.title(Muscles[iM])
+    # ---Band-pass filter signals---
+    low_cut = 10
+    high_cut = 400
+    EMGBP = EMG.meca.band_pass(order=2, cutoff=[low_cut, high_cut], freq=EMG_Freq)
 
-# ---Band-pass filter signals---
-low_cut = 10
-high_cut = 400
-EMGBP = EMG.meca.band_pass(order=2, cutoff=[low_cut, high_cut], freq=EMG_Freq)
+    # fft_EMGBP = EMGBP.meca.fft(EMG_Freq)
+    #
+    # EMGBP.plot(x="time", col="channel", col_wrap=5)
+    # plt.suptitle('band-pass filtered EMG signals', fontsize=16)
+    #
+    # plt.figure(4)
+    # plt.suptitle('FFT band-pass filtered EMG signals', fontsize=16)
+    # for iM in range(len(Muscles)):
+    #     plt.subplot(2, 5, iM + 1)
+    #     fft_EMGBP[iM].plot.line(x="freq")
+    #     plt.title(Muscles[iM])
 
-# fft_EMGBP = EMGBP.meca.fft(EMG_Freq)
-#
-# EMGBP.plot(x="time", col="channel", col_wrap=5)
-# plt.suptitle('band-pass filtered EMG signals', fontsize=16)
-#
-# plt.figure(4)
-# plt.suptitle('FFT band-pass filtered EMG signals', fontsize=16)
-# for iM in range(len(Muscles)):
-#     plt.subplot(2, 5, iM + 1)
-#     fft_EMGBP[iM].plot.line(x="freq")
-#     plt.title(Muscles[iM])
+    # ---stop-band filter signals---
+    # low_cut = 59
+    # high_cut = 61
+    # EMGBS = EMGBP.meca.band_stop(order=2, cutoff=[low_cut, high_cut], freq=EMG_Freq)
 
-# ---stop-band filter signals---
-# low_cut = 59
-# high_cut = 61
-# EMGBS = EMGBP.meca.band_stop(order=2, cutoff=[low_cut, high_cut], freq=EMG_Freq)
+    # fft_EMGBS = EMGBS.meca.fft(EMG_Freq)
+    #
+    # EMGBS.plot(x="time", col="channel", col_wrap=5)
+    # plt.suptitle('stop-band filtered EMG signals', fontsize=16)
+    #
+    # plt.figure(6)
+    # plt.suptitle('FFT stop-band filtered EMG signals', fontsize=16)
+    # for iM in range(len(Muscles)):
+    #     plt.subplot(2, 5, iM + 1)
+    #     fft_EMGBS[iM].plot.line(x="freq")
+    #     plt.title(Muscles[iM])
 
-# fft_EMGBS = EMGBS.meca.fft(EMG_Freq)
-#
-# EMGBS.plot(x="time", col="channel", col_wrap=5)
-# plt.suptitle('stop-band filtered EMG signals', fontsize=16)
-#
-# plt.figure(6)
-# plt.suptitle('FFT stop-band filtered EMG signals', fontsize=16)
-# for iM in range(len(Muscles)):
-#     plt.subplot(2, 5, iM + 1)
-#     fft_EMGBS[iM].plot.line(x="freq")
-#     plt.title(Muscles[iM])
+    # ---Remove mean---
+    EMGBL = EMGBP
+    EMGBL.meca.center()
 
-# ---Remove mean---
-EMGBL = EMGBP
-EMGBL.meca.center()
+    fft_EMGBL = EMGBL.meca.fft(EMG_Freq)
 
-fft_EMGBL = EMGBL.meca.fft(EMG_Freq)
+    EMGBL.plot(x="time", col="channel", col_wrap=5)
+    plt.suptitle('Detrended & filtered EMG signals' + key, fontsize=16)
 
-EMGBL.plot(x="time", col="channel", col_wrap=5)
-plt.suptitle('Detrended & filtered EMG signals', fontsize=16)
+    plt.figure(8)
+    plt.suptitle('FFT detrended & filtered EMG signals' + key, fontsize=16)
+    for iM in range(len(Muscles)):
+        plt.subplot(2, 5, iM + 1)
+        fft_EMGBL[iM].plot.line(x="freq")
+        plt.title(Muscles[iM])
 
-plt.figure(8)
-plt.suptitle('FFT detrended & filtered EMG signals', fontsize=16)
-for iM in range(len(Muscles)):
-    plt.subplot(2, 5, iM + 1)
-    fft_EMGBL[iM].plot.line(x="freq")
-    plt.title(Muscles[iM])
-
-plt.show()
+    plt.show()
+    input("Press Enter to continue...")
 
 # # Store the filtered data into a dictionnary and save as .mat file
 # data_to_keep = EMGBL.data
